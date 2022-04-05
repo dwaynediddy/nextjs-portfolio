a1b6b54d8febce8e191b2ff43d7c5c251da4a5db
dwaynediddy <burrows.dwayne@icloud.com>
2022-04-05 17:40:43 +0200

    codetime component taking data from wakatime with responsiveness added

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,40 @@
 import styles from '../styles/Home.module.css'
 import { useTheme } from "next-themes"
+import styled from 'styled-components'
 
 import ComingSoon from './ComingSoon'
+import CodeTime from '../components/CodeTime'
 
 export default function Home() {
   const { theme, resolvedTheme, setTheme } = useTheme()
+  function handleClick() {
+    setTheme(resolvedTheme === "light" ? "dark" : "light")
+  }
+
   return (
-    <div className={styles.container}>
+    <Container>
       <div>
-          {resolvedTheme !== theme ? `${theme} (${resolvedTheme})` : theme} mode
+          {/* {resolvedTheme !== theme ? `${theme} (${resolvedTheme})` : theme} */}
+        <Button
+          type="checkbox"
+          onClick={handleClick}
+          >
+          {resolvedTheme === "light" ? "dark" : "light"} Mode
+        </Button>
       </div>
         <ComingSoon />
-        <button
-          onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
-        >
-          Toggle {resolvedTheme === "light" ? "dark" : "light"} mode
-        </button>
-    </div>
+        <CodeTime />
+          {/* {resolvedTheme === "🌞" ? "🌜" : "🌞"} */}
+
+    </Container>
   )
 }
+const Container = styled.div`
+  padding: 1rem;
+`
+
+const Button = styled.button`
+  width: 8rem;
+  height: 2rem;
+  border-radius: 2rem;
+`
