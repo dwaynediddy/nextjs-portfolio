923f7b9a0c483635530019ce8bb8d5daaa1d6a19
dwaynediddy <burrows.dwayne@icloud.com>
2022-04-05 15:22:08 +0200

    added dark mode without flicker

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import styles from '../styles/Home.module.css'
+import { useTheme } from "next-themes"
+
 import ComingSoon from './ComingSoon'
 
 export default function Home() {
+  const { theme, resolvedTheme, setTheme } = useTheme()
   return (
     <div className={styles.container}>
+      <div>
+          {resolvedTheme !== theme ? `${theme} (${resolvedTheme})` : theme} mode
+      </div>
         <ComingSoon />
+        <button
+          onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
+        >
+          Toggle {resolvedTheme === "light" ? "dark" : "light"} mode
+        </button>
     </div>
   )
 }
